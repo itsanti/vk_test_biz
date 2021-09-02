7ff42432fdc50cd0357269f3a3c23311ce90b1da
itsanti <kurov.aleksandr+github@gmail.com>
2021-09-02 23:02:36 +0300

    add next components

---
+++ components/adsmain.js
@@ -0,0 +1,40 @@
+export default function Adsmain() {
+    return (
+    <section class="ads__main-screen">
+        <div class="main-screen-container main-screen-container--blue">
+            <div class="main-screen-container-wrapper">
+                <div class="main-screen-content">
+                <div class="main-screen-description">
+                    <h1 class="main-screen-description__title">Реклама ВКонтакте поможет найти клиентов</h1>
+                    <p class="main-screen-description__subtitle main-screen-description__subtitle--desktop">Запускайте рекламу, увеличивайте продажи<br/>и развивайте бизнес. Вперёд, к новым вершинам!</p>
+                    <div class="main-screen-description__button main-screen-description__button--desktop">
+                    <a id="gtm-ads-main-screen-desktop" class="main-screen-description__link link_for_ads_easy_promote" href="https://vk.com/biz/admin?utm_source=vk_biz_adpage&amp;utm_medium=createad_button" target="_blank">Запустить рекламу</a>
+                    </div>
+                </div>
+                </div>
+                <div class="main-screen__img--desktop"></div>
+            </div>
+            </div>
+
+            <div class="main-screen-container">
+            <div class="main-screen-content main-screen-content__numbers">
+                <div class="main-screen-number">
+                <div class="main-screen-number__digit">500</div>
+                <p class="main-screen-number__description main-screen-number__description--money">рублей достаточно для запуска рекламы</p>
+                </div>
+
+                <div class="main-screen-number">
+                <div class="main-screen-number__digit">97</div>
+                <p class="main-screen-number__description main-screen-number__description--users">миллионов активных<br/>пользователей в&nbsp;месяц</p>
+                </div>
+
+                <div class="main-screen-number">
+                <div class="main-screen-number__digit">3</div>
+                <p class="main-screen-number__description main-screen-number__description--time desktop">минуты<br/>для запуска рекламы с&nbsp;мобильного</p>
+                </div>
+            </div>
+        </div>
+    </section>
+ 
+    )
+}
---
+++ components/goal.js
@@ -0,0 +1,41 @@
+export default function Goal() {
+    return (
+    <section class="begin show-mobile-callback-form">
+        <div id="begin-id" class="begin__container container">
+            <h2>Какая у вас цель?</h2>
+
+            <div class="begin__swiper swiper-container">
+            <ul class="begin__list swiper-wrapper">
+
+                <li class="begin__item begin__item--ad swiper-slide begin__item--active">
+                <a id="gtm-37" class="link_for_ads_easy_promote" href="https://vk.com/biz/admin?utm_source=vk_biz_adpage&amp;utm_medium=createad_button" target="_top">
+                    <h3><span>Быстро найти<br/>клиентов</span></h3>
+                    <p>Умные алгоритмы привлекут клиентов на сайт или в&nbsp;сообщество, сэкономив бюджет</p>
+                    <span class="button begin__button button-reset">Найти клиентов</span>
+                </a>
+                </li>
+
+                <li class="begin__item begin__item--shop swiper-slide">
+                <a id="gtm-36" href="https://vk.com/business-pages" target="_top">
+                    <h3><span>Начать бизнес ВКонтакте</span></h3>
+                    <p>Создайте Страницу бизнеса ВКонтакте с&nbsp;магазином и бесплатным сайтом<br/>на её основе</p>
+                    <span class="button button-reset">Начать</span>
+                </a>
+                </li>
+
+                <li class="begin__item begin__item--create swiper-slide">
+                <a id="gtm-35" href="https://vk.com/business-ads" target="_top">
+                    <h3><span>Развивать дело<br/>онлайн</span></h3>
+                    <p>Оцените дополнительные инструменты, которые помогут искать клиентов и&nbsp;автоматизировать бизнес</p>
+                    <span class="button button-reset">Развивать</span>
+                </a>
+                </li>
+
+            </ul>
+            <div class="begin__pagination swiper-pagination"></div>
+            </div>
+
+        </div>
+    </section>
+    )
+}
